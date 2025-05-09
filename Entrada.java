a3fa87f6e450e2ed7483b0130fce151a654b1c8c
Eyve9ykecua7y <20243rd049@utez.edu.mx>
2025-05-09 09:24:58 -0600

    cree un programa con entrada

---
+++ Entrada.java
@@ -0,0 +1,7 @@
+import javax swing JOptionPane;
+public class Entrada{
+    public static void main(String args[]){
+        String nombreUsuario = JOptionPane.showInputDialog("ingresa tu nombre: ");
+        System.out.println("Tu nombre es: "+ nombreUsuario);
+    }
+}
